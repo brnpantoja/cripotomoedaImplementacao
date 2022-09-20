8918e5eb5b38e1a0a08b459b5c3e35a673afe00e
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-09-20 00:55:02 -0300

    Implementando os métodos de entrada e saída na classe de transações

--- src/main/globalsoftwaresupport/cryptocurrency/Transaction.java
+++ src/main/globalsoftwaresupport/cryptocurrency/Transaction.java
@@ -1,5 +1,8 @@
 package main.globalsoftwaresupport.cryptocurrency;
 
+import main.globalsoftwaresupport.blockchain.Blockchain;
+
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,5 +30,120 @@ public class Transaction {
         this.amount = amount;
         this.inputs = inputs;
         calculateHash();
+    }
+
+    public boolean verifyTransaction() {
+        if (!verifySignature()) {
+            System.out.println("Transações inválidas devido a assinatura inválida...");
+            return false;
+        }
+        // reunir transações não gastas (tem que considerar as entradas)
+        for (TransactionInput transactionInput : inputs)
+            transactionInput.setUTXO(Blockchain.UTXOs.get(transactionInput.getTransactionOutputId()));
+
+        // as transações têm 2 partes: enviar um valor para o destinatário + enviar o (valor do saldo)
+        // de volta ao remetente
+        // enviar valor ao destinatário
+        outputs.add(new TransactionOutput(this.receiver, amount, transactionId));
+        // envie o que sobrou de 'mudança' de volta ao remetente
+        outputs.add(new TransactionOutput(this.sender, getInputsSum() - amount, transactionId));
+
+        // TEMOS QUE ATUALIZAR A LISTA UTXO
+        // as saídas serão entradas para outras transações
+        for (TransactionOutput transactionOutput : outputs)
+            Blockchain.UTXOs.put(transactionOutput.getId(), transactionOutput);
+
+        // remover entradas de transação da lista UTXOs do blockchain porque elas foram gastas
+        for (TransactionInput transactionInput : inputs)
+            if (transactionInput.getUTXO() != null)
+                Blockchain.UTXOs.remove(transactionInput.getUTXO().getId());
+
+        return true;
+
+    }
+
+    // é assim que calculamos quanto dinheiro o remetente tem
+    // temos que considerar as transações no pas
+    private double getInputsSum() {
+        double sum = 0;
+
+        for (TransactionInput transactionInput : inputs)
+            if (transactionInput.getUTXO() != null)
+                sum += transactionInput.getUTXO().getAmount();
+        return sum;
+
+    }
+
+    public void generateSignature(PrivateKey privateKey) {
+        String data = sender.toString() + receiver.toString() + Double.toString(amount);
+        signature = CryptographyHelper.sign(privateKey, data);
+
+    }
+
+    public boolean verifySignature() {
+        String data = sender.toString() + receiver.toString() + Double.toString(amount);
+        return CryptographyHelper.verify(sender, data, signature);
+
+    }
+
+    private void calculateHash() {
+        String hashData = sender.toString() + receiver.toString() + Double.toString(amount);
+        this.transactionId = CryptographyHelper.generateHash(hashData);
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public PublicKey getSender() {
+        return sender;
+    }
+
+    public void setSender(PublicKey sender) {
+        this.sender = sender;
+    }
+
+    public PublicKey getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(PublicKey receiver) {
+        this.receiver = receiver;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public byte[] getSignature() {
+        return signature;
+    }
+
+    public void setSignature(byte[] signature) {
+        this.signature = signature;
+    }
+
+    public List<TransactionInput> getInputs() {
+        return inputs;
+    }
+
+    public void setInputs(List<TransactionInput> inputs) {
+        this.inputs = inputs;
+    }
+
+    public List<TransactionOutput> getOutputs() {
+        return outputs;
+    }
+
+    public void setOutputs(List<TransactionOutput> outputs) {
+        this.outputs = outputs;
     }
 }
